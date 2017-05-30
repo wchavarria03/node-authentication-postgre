840422f930f5336b82ba62695f9108a7e17df080
Walter Chavarria <wchavarria03@gmail.com>
2017-05-29 22:24:54 -0600

    Added userId when login

--- server/api/auth/controller.js
+++ server/api/auth/controller.js
@@ -2,5 +2,8 @@ const signToken = require('./auth').signToken;
 
 exports.signin = (req, res, next) => {
   // Create a token and send it back for the client to consume
-  res.json({token: signToken(req.user.id)});
+  res.json({
+    token: signToken(req.user.id),
+    userId: req.user.id
+  });
 };
